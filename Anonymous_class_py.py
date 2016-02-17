ee61200b8e415d5dbb8906a6ef7652a0dcd7b043
Christopher Mckerracher <ChrisLMckerracher@gmail.com>
2016-02-17 16:34:48 -0500

    Update Anonymous_class_py.py

--- Anonymous_class_py.py
+++ Anonymous_class_py.py
@@ -5,7 +5,7 @@ def create_anon_class(*args):
     #otherwise, just having a string of a attribute will set it's default value
     #to its own name
 
-    return (lambda z,*y:type.__new__(type,z,(object,),(lambda f:apply(lambda\
+    return (lambda *y:type.__new__(type,"",(object,),(lambda f:apply(lambda\
     *d:map(lambda e:d[0].update(e) or d[0],d[1:])[0],f) if len(f)>1 else f[0])\
     (map(lambda b:b if type(b)==dict else{str(b):b},y))))(*args)
 
