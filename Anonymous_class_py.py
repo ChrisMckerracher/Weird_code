8fa64cf6a84a4df1257cbec18867c3386911818f
Christopher Mckerracher <ChrisLMckerracher@gmail.com>
2016-08-26 15:51:29 -0400

    Did I even test the previous code before committing?

--- Anonymous_class_py.py
+++ Anonymous_class_py.py
@@ -1,13 +1,18 @@
-def create_anon_class(*args):
+
+def create_anon_class(*args, **kwargs):
     #define arguments such as, function names are in some dictionary.
     #all attributes can be put into a dictionary, and thus their default values
     #can be assigned.
     #otherwise, just having a string of a attribute will set it's default value
     #to its own name
 
-    return (lambda *y:type.__new__(type,"",(object,),(lambda f:apply(lambda\
-    *d:map(lambda e:d[0].update(e) or d[0],d[1:])[0],f) if len(f)>1 else f[0])\
-    (map(lambda b:b if type(b)==dict else{str(b):b},y))))(*(args + (kwargs,)))
+    return (lambda *y:type.__new__(type,"",(object,),(lambda f:[(lambda:(f[0].\
+            update(f.pop())))() for x in range(len(f)-1)] and f if len(f)>0 \
+            else f)([(lambda x:x if type(x)==dict else {str(x):x})(x) for x \
+            in y])[0]))(*(args + (kwargs,)))
 
 
 create_anon_class('spec',{'getspec':lambda self: print(self.spec)}, test='hello world', gettest=(lambda self: print(self.test)))().gettest()
+
+
+    
